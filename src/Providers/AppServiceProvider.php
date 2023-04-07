cd608f2e297e603770f2cfba7e462c37cc807b93
Susheel Kumar Singh <susheelkrsingh306@gmail.com>
2023-04-07 13:44:58 +0530

    timezone updated from service provider insteed of publishing config/app.php

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Config::set('app.timezone', 'Asia/Kolkata');
         Paginator::useBootstrapFive();
         // $settings = Setting::where('id', 1)->first();
         // Config::set('settings', $settings);
