a3c8c6e7823e415bff0060bab3c38fc000931388
Susheel Kumar Singh <susheelkrsingh306@gmail.com>
2025-12-25 00:42:59 +0530

    installation error fixed

--- src/common/Commands/Helper/InstallPackages.php
+++ src/common/Commands/Helper/InstallPackages.php
@@ -18,7 +18,6 @@ class InstallPackages
         $this->installPackage($this_data, ['npm', 'install', 'sweetalert2'], "NPM package: sweetalert2");
         $this->installPackage($this_data, ['npm', 'install', 'react-select'], "NPM package: react-select");
         $this->installPackage($this_data, ['composer', 'require', 'spatie/laravel-permission'], "Composer package: spatie/laravel-permission");
-        $this->installPackage($this_data, ['composer', 'require', 'spatie/laravel-permission'], "Composer package: spatie/laravel-permission");
         $this->installPackage($this_data, ['composer', 'require', 'spatie/laravel-medialibrary'], "Composer package: spatie/laravel-medialibrary");
         $this->installPackage($this_data, ['composer', 'require', 'intervention/image'], "Composer package: intervention/image");
         $this->installPackage($this_data, ['composer', 'require', 'tightenco/ziggy'], "Composer package: tightenco/ziggy");
